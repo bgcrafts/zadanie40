7a8a3d3db9ff7414fd7b1e65e1933c7261b68f4b
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-13 18:53:37 +0100

    zadanie40

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 
-
-
-
 let clickButton = document.querySelector('button');
 clickButton.onclick = function () {
     let imie = document.getElementById("imie").value;
-    
 
 
-    return document.getElementById("name").innerHTML = imie;
 
-    
+    document.getElementById("name").innerHTML = imie;
+
+    let nazwisko = document.getElementById("nazwisko").value;
+    document.getElementById("surname").innerHTML = nazwisko;
+
+    let nrtelefonu = document.getElementById("nrtel").value;
+    document.getElementById("phone").innerHTML = nrtelefonu;
+
+
 }
