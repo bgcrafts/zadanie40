681c6a035d7b18281b61091fe97d04d57d820cd7
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-13 14:10:51 +0100

    projekt

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+
+let clickButton = document.querySelector('button');
+clickButton.onclick = function () {
+    let imie = document.getElementById("imie").value;
+    
+
+
+    return document.getElementById("name").innerHTML = imie;
+
+    
+}
